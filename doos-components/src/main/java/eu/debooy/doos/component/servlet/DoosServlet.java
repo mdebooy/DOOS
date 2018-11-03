96e320c9243b6ccb2f126e7e262a9aa0e713a689
Marco DE BOOIJ <marco.developer@debooy.eu>
2018-11-03 21:45:28 +0100

    Servlet voor DOOS applicaties.

---
+++ doos-components/src/main/java/eu/debooy/doos/component/servlet/DoosServlet.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2018 Marco de Booij
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence");
+ * you may not use this work except in compliance with the Licence. You may
+ * obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and
+ * limitations under the Licence.
+ */
+package eu.debooy.doos.component.servlet;
+
+import eu.debooy.doos.component.I18nTeksten;
+import eu.debooy.doos.component.Properties;
+import eu.debooy.doosutils.errorhandling.exception.ObjectNotFoundException;
+import eu.debooy.doosutils.service.CDI;
+
+import javax.servlet.http.HttpServlet;
+
+
+/**
+ * @author Marco de Booij
+ */
+public class DoosServlet extends HttpServlet {
+  private static final  long    serialVersionUID  = 1L;
+
+  private I18nTeksten i18nTekst = null;
+  private Properties  property  = null;
+
+  private I18nTeksten getI18nTekst() {
+    if (null == i18nTekst) {
+      i18nTekst = (I18nTeksten) CDI.getBean(I18nTeksten.class);
+    }
+
+    return i18nTekst;
+  }
+
+  protected String getParameter(String parameter) {
+    String  waarde;
+    try {
+      waarde  = getProperty().value(parameter);
+    } catch (ObjectNotFoundException e) {
+      return "";
+    }
+
+    return waarde;
+  }
+
+  private Properties getProperty() {
+    if (null == property) {
+      property  = (Properties) CDI.getBean(Properties.class);
+    }
+
+    return property;
+  }
+
+  protected String getTekst(String tekst) {
+    return getI18nTekst().tekst(tekst);
+  }
+
+}
