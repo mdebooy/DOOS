ce720a129b4b5864ce72dd00f60681e03fe410b9
Marco DE BOOIJ <marco.developer@debooy.eu>
2023-01-21 20:21:44 +0100

    Sonar aanbevelingen.

--- doos-web/src/main/java/eu/debooy/doos/domain/TaalDto.java
+++ doos-web/src/main/java/eu/debooy/doos/domain/TaalDto.java
@@ -240,6 +240,7 @@ public class TaalDto extends Dto implements Comparable<TaalDto> {
     }
   }
 
+  @SuppressWarnings("common-java:DuplicatedBlocks")
   public void setIso6391(String iso6391) {
     if (null == iso6391) {
       this.iso6391  = null;
